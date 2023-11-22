ec4b2db34ec933b5f98d9c85531c53bcecbd71df
Carla Soares <csmsoares0@gmail.com>
2023-11-22 20:10:52 +0000

    Consulta simples com query methods

--- src/main/java/com/cs/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/cs/workshopmongo/repositories/PostRepository.java
@@ -1,9 +1,14 @@
 package com.cs.workshopmongo.repositories;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
 import com.cs.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
